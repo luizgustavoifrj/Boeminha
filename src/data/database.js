d45bf84bce16632f909f98cb15d4e4707496588b
joevjoao-sys <joevjoao@gmail.com>
2026-07-07 17:02:41 -0300

    Update database.js

--- src/data/database.js
+++ src/data/database.js
@@ -90,7 +90,7 @@ export const atracoesDb = [
   },
   {
     id: "parque-cidade",
-    titulo: "Parqueeke da Cidade",
+    titulo: "Parqueee da Cidade",
     categoria: "natureza",
     tag: "Pôr do Sol",
     subtopicos: ["Ar Livre", "Parapente", "Vista Rio"],
